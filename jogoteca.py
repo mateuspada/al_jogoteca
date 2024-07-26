97945f11669c23186d184721b03bbc55dfb97813
Mateus Spada Leme <mateuspada@live.com>
2024-07-25 21:31:59 -0300

    feat: created endpoint to add a game, and also bootstrap style (css)

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 class Jogo:
     def __init__(self, nome, categoria, console):
@@ -6,15 +6,31 @@ class Jogo:
         self.categoria=categoria
         self.console=console
 
+jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
+jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
+lista_jogos = [jogo1, jogo2, jogo3]
+
 app = Flask(__name__)
 
-@app.route('/inicio')
-def hello_world():
-    jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
-    jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
-    jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
-    lista_jogos = [jogo1, jogo2, jogo3]
-
+@app.route('/')
+def index():
     return render_template('lista.html', titulo='Jogos', jogos=lista_jogos)
 
-app.run(port=8080)
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo')
+
+@app.route('/criar', methods=['POST'])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+
+    jogo = Jogo(nome, categoria, console)
+    lista_jogos.append(jogo)
+
+    return redirect('/')
+
+
+app.run(port=8080, debug=True)
